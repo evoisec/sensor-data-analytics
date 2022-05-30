bef5ff2c292830effaa91ea1a33565a5b8b101e0
evo <evo.eftimov@isecc.com>
2022-05-30 01:03:50 +0100

    initial commit

--- src/main/python/binary_data_extractor.py
+++ src/main/python/binary_data_extractor.py
@@ -102,7 +102,7 @@ def main():
         print(key, value)
         print(sensor_dict[key])
 
-    #extract_label(sensor_dict)
+    extract_label(sensor_dict)
     extract_acc(sensor_dict)
     #extract_activity(sensor_dict)
 
