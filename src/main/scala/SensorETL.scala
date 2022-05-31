6515887f838faa14783ba06c55519c9fd51667e2
evo <evo.eftimov@isecc.com>
2022-05-31 14:03:21 +0100

    initial commit

--- src/main/scala/SensorETL.scala
+++ src/main/scala/SensorETL.scala
@@ -4,7 +4,7 @@ object SensorETL {
 
   def main(args: Array[String]): Unit = {
 
-    //Client Transaction (timeseries) Datasets
+    //"Client Transaction" (sensor timeseries) Datasets
     val tsIndexFilePath = "E:\\project\\data\\PPG_FieldStudy\\S1\\S1-PKL-CSV\\PKL-ECG-LABEL-MAIN-INDEX-INDEXED.csv"
     val tsActivityFilePath = "E:\\project\\data\\PPG_FieldStudy\\S1\\S1-PKL-CSV\\PKL-ACTIVITY-ENHANCED-TS-INDEXED.csv"
 
